8d57a14af94ca5e6f381d305bfa1eab968663347
thupx <xuanthu404@gmail.com>
2019-10-30 11:05:57 +0700

    adjust exception response

--- src/ctf-events/events.controller.ts
+++ src/ctf-events/events.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Req, Res, Query } from "@nestjs/common";
+import { Controller, Get, Post, Body, Req, Res, Query, Param, NotFoundException, InternalServerErrorException } from "@nestjs/common";
 import { Request, Response } from 'express';
 import { CTFEventsService } from "./events.service";
 import { CreateCTFEventDto } from "./dtos/create-ctf-event.dto";
@@ -9,28 +9,39 @@ export class CTFEventsController {
     async getEvents(@Req() req: Request, @Res() res: Response) {
         try {
             const events = await this.CTFEventsService.getCTFEvents();
-            res.json(events);
+            if (events)
+                return res.json(events);
+            else throw new NotFoundException("Can not find all events");
         }
         catch (err) {
             console.log("CTFEventsController/Get/api/v1/events error", err);
+            throw new InternalServerErrorException();
         }
     }
-    @Get("/events")
-    async getEvent(@Query('id') id: string, @Res() res: Response) {
+    @Get("/events/:id")
+    async getEvent(@Req() req: Request, @Res() res: Response) {
+        const id = req.params.id;
         try {
             const event = await this.CTFEventsService.getCTFEvent(id);
-            res.json(event);
+            if (event)
+                return res.json(event);
+            else throw new NotFoundException("Not found event");
         } catch (error) {
             console.log("CTFEventsController/Get/api/v1/events query failed", error);
+            if (error.status !== 404)
+                throw new InternalServerErrorException(`CTFEventsController/Get/api/v1/events/${id} query failed`);
+            throw error;
         }
     }
     @Post('/events')
-    async createEvent(@Res() res: Response, @Body() event: CreateCTFEventDto) {
+    async createEvent(@Res() res: Response, @Body() event) {
         try {
-            const result = await this.CTFEventsService.createCTFEvent(event);
+            const data = new CreateCTFEventDto({ ...event });
+            const result = await this.CTFEventsService.createCTFEvent(data);
             if (result) res.json(result)
         } catch (error) {
             console.log("CTFEventsController/Post/api/v1/events command failed", error);
+            throw new InternalServerErrorException();
         }
     }
 }

a4939f35b17ac6e9b243f977bd92a9bb4f5640d3
thupx <xuanthu404@gmail.com>
2019-10-31 15:33:41 +0700

    refactor

--- src/teams/schemas/team.schema.ts
+++ src/teams/schemas/team.schema.ts
@@ -5,7 +5,7 @@ import { Schema } from 'mongoose';
 export const TeamSchema = new mongoose.Schema({
     teamName: String,
     leader: Schema.Types.ObjectId,
-    members: [{type: Schema.Types.ObjectId, ref: 'User'}],
-    eventsRegistration: [{type: Schema.Types.ObjectId, ref: 'EventRegistration'}],
+    members: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    eventsRegistration: [{ type: Schema.Types.ObjectId, ref: 'EventRegistration' }],
     teamEventStatusId: [{ type: Schema.Types.ObjectId, ref: 'TeamEventStatus' }],
-}, {timestamps: true}) ;
+}, { timestamps: true });

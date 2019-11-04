e85361f05490c785917cb4bc001178991fa69102
thinhnnd <16521179@gm.uit.edu.vn>
2019-11-04 16:17:55 +0700

    change port 5000 -->80

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as dotenv from 'dotenv';
 import { Logger, ValidationPipe } from '@nestjs/common';
 
 dotenv.config();
-const port = process.env.PORT || 5050;
+const port = process.env.PORT || 5000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ disableErrorMessages: true }));
